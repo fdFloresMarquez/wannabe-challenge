a8af3899617bf68175e47d0ab99abb87acdb1024
Facundo <floresfacundo68@gmail.com>
2022-11-08 19:14:20 -0300

    add CharacterCard component to pages

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -1,7 +1,37 @@
-type Props = {};
+import { GetServerSideProps } from 'next';
 
-const Character: React.FC<Props> = () => {
-  return <div>{`<Character />`}</div>;
+import { Character } from '@/types/characters';
+
+type Props = {
+  character: Character;
 };
 
-export default Character;
+const CharacterCard: React.FC<Props> = ({ character }) => {
+  return (
+    <div className="card mt-5">
+      <h5 className="card-header card-title">{character.name}</h5>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Gender: {character.gender}</li>
+        <li className="list-group-item">Mass: {character.mass}</li>
+        <li className="list-group-item">Height: {character.height}</li>
+        <li className="list-group-item">Eye Color: {character.eye_color}</li>
+        <li className="list-group-item">Birth Year: {character.birth_year}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CharacterCard;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+
+  const response = await fetch(`https://swapi.dev/api/people/${id}`);
+  const data: Character = await response.json();
+
+  return {
+    props: {
+      character: data,
+    },
+  };
+};
