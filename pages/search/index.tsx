81bf7f03ffb61e291d05ae1342884749a976dc2a
Facundo <floresfacundo68@gmail.com>
2022-11-08 19:14:29 -0300

    add search component to pages

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,5 +1,33 @@
-const Search: React.FC = () => {
-  return <div>{`<Search />`}</div>;
+import { GetServerSideProps } from 'next';
+
+import { Character, GetCharactersResults } from '@/types/characters';
+import CharactersList from '@/components/CharactersList';
+
+type Props = {
+  characters: Character[];
+};
+
+const Search: React.FC<Props> = ({ characters }) => {
+  return (
+    <>
+      <CharactersList characters={characters} title={`Search Results:`} />
+    </>
+  );
 };
 
 export default Search;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const searchQuery = context.query.q;
+
+  const response = await fetch(`https://swapi.dev/api/people/?search=${searchQuery}`);
+  const { previous, next, results }: GetCharactersResults = await response.json();
+
+  return {
+    props: {
+      characters: results,
+      previous,
+      next,
+    },
+  };
+};
